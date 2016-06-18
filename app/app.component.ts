61c9d59c3732297dc5025ab9c44162a2b318e002
jychoi <jychoi@g2gsolution.co.kr>
2016-06-18 21:32:02 +0900

    appname=choijy

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'my-app2',
+  selector: 'my-app-choijy',
   template: `
     <h1>{{title}}</h1>
     <nav>
