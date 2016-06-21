bfe017a036eeac4d37301f127cc6f7efc3468c31
jychoi <jychoi@g2gsolution.co.kr>
2016-06-21 13:43:17 +0900

    first github

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'my-app-choi',
+  selector: 'my-app-github',
   template: `
     <h1>{{title}}</h1>
     <nav>
