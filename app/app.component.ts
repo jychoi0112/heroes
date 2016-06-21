0b794f1ccd670b19eef5312f3deb87bcfb916332
jychoi <jychoi@g2gsolution.co.kr>
2016-06-21 13:52:04 +0900

    Modify by maxeffect.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'my-app-github',
+  selector: 'my-app-maxeffect',
   template: `
     <h1>{{title}}</h1>
     <nav>
